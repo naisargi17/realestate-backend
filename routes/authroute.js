ef11f1d609f672258f28ace6393407c67e10e8c9
naishub <patelnaisargi74@gmail.com>
2024-04-07 00:16:29 +0530

    goog auth

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login, signup } from "../controller/authcontoller.js";
+import { google, login, signup } from "../controller/authcontoller.js";
 const router = express.Router();
 
 router.post("/signup",signup);
 router.post("/login",login);
+router.post("/google",google)
 
 export default router;
