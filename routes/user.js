3dc06098e223520cc51c0e6bf916ccf7ecd51880
naishub <patelnaisargi74@gmail.com>
2024-04-07 14:54:08 +0530

    listing create

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { test } from "../controller/usercontroller.js";
 import { verifyToken } from "../utils/verifyUser.js";
-import { updateUser } from "../controller/usercontroller.js";
+import { updateUser,deleteUser } from "../controller/usercontroller.js";
 const router = express.Router();
 
 router.get('/test',test);
 router.post('/update/:id', verifyToken,updateUser)
+router.delete('/delete/:id', verifyToken,deleteUser)
 export default router;
