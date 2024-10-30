1a3fac170c9692789a920f445216937dd77527a0
naishub <patelnaisargi74@gmail.com>
2024-10-30 21:06:26 +0530

    Enhanced the background

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-import express from "express";
-import mongoose from "mongoose";
+
 import userRouter from "./routes/user.js";
 import authRouter from "./routes/authroute.js";
-import listingRouter from './routes/listingroute.js'
+import listingRouter from './routes/listingroute.js';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
-const app = express();
 import path from "path";
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 3000;
+
+dotenv.config();
 
 mongoose
-  .connect("mongodb+srv://patelnaisargi74:naisargi123@cluster0.vjrdl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .connect("mongodb+srv://patelnaisargi74:naisargi123@cluster0.8rpesqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
-  .catch((err) => console.log("error"));
+  .catch((err) => console.log(err));
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-  app.use(cors(
-    {
-      origin:"http://localhost:5173",
-      credentials: true
-    }
-  ));
-  app.use(express.json());
-  app.use(cookieParser());
-  
+app.use(cors({
+  origin: "http://localhost:3000",  // Allow requests from Vite server
+  credentials: true
+}));
 
-  app.use('/api/user',userRouter);
-  app.use('/api/auth',authRouter);
-  app.use('/api/listing',listingRouter);
+app.use(express.json());
+app.use(cookieParser());
 
-  app.use(express.static(path.join(__dirname,'/client/dist')));
+// Set up routes
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-  app.get('*', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "client", "build")));
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// Serve static files from client build folder
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
- 
-
-
-  app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    });
-  })
+});
